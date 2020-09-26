edaeba831799ad718830d570ee2cfe083a57aaeb
Joan C. Ayala <jflaco85@gmail.com>
2020-09-25 20:59:32 -0500

    Suma de 3 números

---
+++ sumas.py
@@ -0,0 +1,21 @@
+#Script: sumas
+'''
+Suma de 3 números
+ingresados por el usuario
+'''
+
+#INPUTS
+print("Ingrese n1: ")
+n1 = int(input())
+
+print("Ingrese n2: ")
+n2 = int(input())
+
+print("Ingrese n3: ")
+n3 = int(input())
+
+#PROCESS
+suma = n1 + n2 + n3
+
+#OUTPUT
+print("Resultado es: ", suma)
